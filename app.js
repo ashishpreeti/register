6554c055f85ff1ecc2b4c6664cb65e3113d10ea4
ashish patil <patilas@visa.com>
2016-07-09 07:50:32 +0100

    setup mongo and add an entry

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mongodb = require('mongodb').MongoClient;
 
 var app = express();
 
@@ -11,9 +12,24 @@ app.get('/', function(req, res){
     res.send('Hello World');
 });
 
+var row = {
+    name: 'John Doe',
+    age: '25',
+    sex: 'Male',
+    country: 'UK'
+};
 
 app.get('/register', function(req, res){
-    res.send('Some useful government service');
+    var url = 'mongodb://localhost:27017/registerApp';
+    mongodb.connect(url, function (err, db) {
+        var collection = db.collection('register');
+        collection.insertOne(row, function (err, results) {
+            console.log("sending the data to db here"+ JSON.stringify(results));
+            res.send(results);
+            db.close();
+        } )
+    } );
+
 });
 
 
