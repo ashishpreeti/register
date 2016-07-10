cca1dcbc52a59a814368b54c2deeee4f1a03fc0e
ashish patil <patilas@visa.com>
2016-07-10 18:12:35 +0100

    make browserify work from command line

--- public/main.js
+++ public/main.js
@@ -1,5 +1,2 @@
 angular.module('registerApp', ['registerApp'])
-    .controller('registerCtrl', function ($scope) {
-        console.log("in the angular controllers");
-        $scope.name = "jone doe";
-    });
+    .controller('registerCtrl', ['$scope', require('./controllers/registerCtrl')]);
