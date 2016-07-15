f0e41f0adcad229778933c69cffe562c89c7a677
ashish patil <patilas@visa.com>
2016-07-15 18:05:25 +0100

    routing to different views.

--- public/main.js
+++ public/main.js
@@ -1,18 +1,30 @@
 angular.module('registerApp', ['ui.router','registerApp'])
     .service('countryService', ['$http', '$q', require('./services/countryService')])
     .controller('registerCtrl', ['$scope', 'countryService', require('./controllers/registerCtrl')])
-    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    .config(['$logProvider','$stateProvider', '$urlRouterProvider', function($logProvider, $stateProvider, $urlRouterProvider) {
         // Categories state routing
         $urlRouterProvider.otherwise('/');
         $stateProvider
             .state('home', {
                 url : '/',
                 templateUrl: 'views/home.html',
-                controller : 'registerCtrl'
+                controller : 'registerCtrl',
+                onEnter: function ($log) {
+                    $log.debug('Entering the home state.');
+                },
+                onExit: function ($log) {
+                    $log.debug('Exiting the home state.');
+                }
             })
             .state('register',{
-                url : 'register',
-                template : '<h1>Application Submitted</h1>'
+                url : '/register',
+                template : '<h1>Application Submitted</h1>',
+                onEnter: function ($log) {
+                    $log.debug('Entering the registered state.');
+                },
+                onExit: function ($log) {
+                    $log.debug('Exiting the registered state.');
+                }
             })
 
     }]);
